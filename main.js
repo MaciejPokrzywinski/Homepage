8e4d457b820ec7982b2dcc0f42bf4a93171e8ba2
Maciej Pokrzywinski <maciej.pokrzywinski@gmail.com>
2019-06-17 21:51:49 +0200

    add a few test querySelector/Learning how to use querySelector

--- main.js
+++ main.js
@@ -1,4 +1,14 @@
+console.log('Name:  ${Test}')
 console.log('Hello there')
 
 console.log('General Kennobi!')
-console.image("https://i.pinimg.com/originals/40/74/60/407460925c9e419d82b93313f0b42f71.jpg");
+//console.image("https://i.pinimg.com/originals/40/74/60/407460925c9e419d82b93313f0b42f71.jpg");
+
+const heading = document.querySelector('#headerLogo');
+const paragraphs = document.querySelectorAll('p');
+
+console.log(heading);
+console.log(paragraphs);
+console.log(paragraphs[1]);
+console.log(paragraphs[1].innerHTML);
+paragraphs[1].innerHTML = '<b>Pozdro</b> JS!';
